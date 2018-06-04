8c4f558149b0dad9bba5320f715ef4fd3c33aea0
Unknown <definiteliyuntao@gmail.com>
2018-06-04 21:46:46 +0200

    Add the export function

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_file
 mapbox_access_token = "pk.eyJ1IjoiaXNwYnMiLCJhIjoiY2poaG5tcG1qMDFqZzM5bnJwZTEwdmV3NCJ9.8v27PGqngZL_uBNrBqXL0A"
+
+import os
+import settings
 
 app = Flask(__name__)
 
@@ -14,6 +17,14 @@ def index():
 def about():
     return render_template('haiti.html')
 
+@app.route('/get-file')
+def get_file():
+    d = os.path.join(settings.STATIC_ROOT, 'result.pdf')
+    return send_file(d, attachment_filename = 'result.pdf')
+
+# @app.route('/haiti')
+# def export():
+#     return render_template('haiti.html')
 
 # @app.route('/service')
 # def services():
---
+++ settings.py
@@ -0,0 +1,9 @@
+import os
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = 'staticfiles'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
